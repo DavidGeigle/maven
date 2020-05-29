d8663c08e18dc828f9bd3d92a1e2f0186a91e366
David Geigle <david.geigle@hs-furtwangen.de>
2020-05-29 15:43:07 +0200

    added files

--- src/main/java/de/hfu/Uppercase.java
+++ src/main/java/de/hfu/Uppercase.java
@@ -1,3 +1,10 @@
+/**
+ * A Programm that changes all characters to Uppercase
+ *
+ * @autor David Geigle
+ * @version 1.0
+ */
+
 package de.hfu;
 
 import java.io.BufferedReader;
@@ -5,6 +12,12 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 class Uppercase {
+	/**
+	 * Main
+	 * Reads Lines via BufferReader and changes them to Uppercase
+	 * @param args
+	 * @throws IOException
+	 */
 	public static void main(String[] args) throws IOException{
 		BufferedReader r = new BufferedReader(new InputStreamReader(System.in));
 		String s = r.readLine();
